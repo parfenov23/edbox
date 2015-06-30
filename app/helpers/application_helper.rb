d1c5a0d5da0e6d3bb7e508d415dab429f4c938db
maks <prospu@qip.ru>
2015-06-30 17:52:28 +0500

    add def ava

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def local_time(time)
     time + (User.time_zone).hour
   end
+
+  def default_img(img)
+    unless img.nil?
+      "data:image/gif;base64,#{img}"
+    else
+      "/images/ava.png"
+    end
+  end
 end
