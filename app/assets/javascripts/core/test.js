b64ada21318f0847f67c10d0438647e10dd88718
GIGrave <gigrave@rambler.ru>
2015-07-21 10:47:37 +0600

    add makeup

--- app/assets/javascripts/core/test.js
+++ app/assets/javascripts/core/test.js
@@ -1,24 +1,8 @@
 $(document).ready(function () {
     $("img:last").load(function(){
-        $('#navigation .btnCurrent').first().addClass("selected");
-        $('#navigation').show();
-        $("#formElem fieldset").first().show();
-
-        $('#navigation .btnCurrent').click(function (e) {
-            var btn = $(this);
-            var id = btn.data("id");
-
-            $('#navigation .btnCurrent.selected').removeClass("selected");
-            btn.addClass("selected");
-
-            $("#formElem fieldset").hide();
-            $("#formElem fieldset.step" + id).show();
+        $("#tests #submit").click(function (e) {
             e.preventDefault();
-        });
-
-        $("#navigation #submit").click(function (e) {
-            e.preventDefault();
-            var form =  $("#formElem");
+            var form =  $("#tests");
             var data = form.serialize();
             $.ajax({
                 type   : 'POST',
