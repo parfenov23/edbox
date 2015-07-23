93a566426074a4c2d46d9a74f10c1f24c5350564
maks <prospu@qip.ru>
2015-07-23 15:56:42 +0500

    fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -31,7 +31,7 @@ module Api::V1
     def update
       user = current_user
       permit_params = user_params.compact
-      permit_params.delete(:password) if permit_params[:password].to_s.length == 0
+      permit_params.delete(:password) unless permit_params[:password].present?
       permit_params[:director] = user.director
 
       if user.valid?
