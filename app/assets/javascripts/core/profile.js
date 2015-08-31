8937bdfe3328b8247e9892f2aa0323230de868fa
maks <prospu@qip.ru>
2015-08-31 19:17:09 +0500

    ED-251

--- app/assets/javascripts/core/profile.js
+++ app/assets/javascripts/core/profile.js
@@ -1,12 +1,12 @@
 profileDataChange = function () {
     $('.profile__main #submit').click(function () {
+
         var data = $('.profile__main').serialize();
         $.ajax({
             type: 'POST',
             url : '/api/v1/users',
             data: data
         }).success(function () {
-            console.log('111');
             show_error('Успешно сохранено', 3000);
         }).error(function () {
             console.log('111');
@@ -15,34 +15,59 @@ profileDataChange = function () {
     });
 };
 
+var passValid = function (btnClick) {
+    var pass = $('#profile input[name=password]').val();
+    var pass_repeat = $('#profile input[name=password_repeat]').val();
+    var validate = false;
+    $('#profile input[name=password], #profile input[name=password_repeat]').removeClass('error');
+    if (pass === pass_repeat){
+        $('#profile input[name=password_repeat]').removeClass('error');
+        $('#profile input[name=password]').removeClass('error');
+        validate = true;
+    } else {
+        validate = false;
+        if (pass_repeat.length){
+            $('#profile input[name=password_repeat]').addClass('error');
+        } else {
+            $('#profile input[name=password_repeat]').removeClass('error');
+        }
+    }
+    if (btnClick == true){
+        if (! pass_repeat.length){
+            validate = false;
+            $('#profile input[name=password_repeat]').addClass('error');
+        }
+        if (!pass.length){
+            validate = false;
+            $('#profile input[name=password]').addClass('error');
+        }
+    }
+    return validate
+};
+
 profilePasswordChangeValidation = function () {
     $('#profile input').blur(function () {
-        var pass = $('#profile input[name=password]').val();
-        var pass_repeat = $('#profile input[name=password_repeat]').val();
-        if (pass === pass_repeat){
-            $('#profile input[name=password_repeat]').removeClass('error');
-        } else {
-            if (pass_repeat.length){
-                $('#profile input[name=password_repeat]').addClass('error');
-            } else {
-                $('#profile input[name=password_repeat]').removeClass('error');
-            }
-        }
+        passValid();
     });
 };
 
 profilePasswordChange = function () {
     $('.profile__password #submit').click(function () {
-        var data = $('.profile__password input[name=password]').serialize();
-        $.ajax({
-            type: 'POST',
-            url : '/api/v1/users/change_password',
-            data: data
-        }).success(function () {
-            show_error('Успешно сохранено', 3000);
-        }).error(function () {
-            show_error('Ошибка', 3000);
-        });
+        if (passValid(true)){
+            var data = $('.profile__password input[name=password]').serialize();
+            $.ajax({
+                type: 'POST',
+                url : '/api/v1/users/change_password',
+                data: data
+            }).success(function () {
+                show_error('Успешно сохранено', 3000);
+            }).error(function () {
+                show_error('Ошибка', 3000);
+            });
+        }else{
+            show_error('Проверьте пароль', 3000);
+        }
+
     });
 };
 
