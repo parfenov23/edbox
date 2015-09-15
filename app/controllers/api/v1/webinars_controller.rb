3f4d8d5e1fddb7d174dbc32c0a2c828672ba1441
maks <prospu@qip.ru>
2015-09-15 12:51:38 +0500

    fix time webinar

--- app/controllers/api/v1/webinars_controller.rb
+++ app/controllers/api/v1/webinars_controller.rb
@@ -4,14 +4,17 @@ module Api::V1
     def update
       webinar = find_webinar
       webinar_params_permit = webinar_params
-      webinar_params_permit[:date_start] = (Time.parse(webinar_params_permit[:date_start])) if webinar_params_permit[:date_start].present?
+      if webinar_params_permit[:date_start].present?
+        curr_time = Time.parse(webinar_params_permit[:date_start]).getlocal("-03:00")
+        webinar_params_permit[:date_start] = curr_time
+      end
       webinar.update(webinar_params_permit)
       # binding.pry
       render json: webinar.transfer_to_json
     end
 
     def all_leading
-      render json: find_webinar.ligament_leads.map{|ll| ll.user.id}
+      render json: find_webinar.ligament_leads.map { |ll| ll.user.id }
     end
 
     def add_leading
