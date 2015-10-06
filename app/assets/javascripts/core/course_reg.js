1ca6de3c6ecc62d871f50cb0857b2854482a58ae
GIGrave <gigrave@rambler.ru>
2015-10-06 11:11:00 +0600

    586 ref

--- app/assets/javascripts/core/course_reg.js
+++ app/assets/javascripts/core/course_reg.js
@@ -4,6 +4,22 @@ var openFormRegistration = function(){
     $(document).scrollTop( popup.offset().top);
 };
 
+var regFromLanding = function(regtype){
+    if ($('.js_openFormRegistration').length != 0){
+        $.when(openFormRegistration()).then(function () {
+            if (regtype.data('type') == 'pers'){
+                $("#formCourseRegPopUp .pidor__raz .btn").click();
+            }
+            if (regtype.data('type') == 'corp'){
+                $("#formCourseRegPopUp .pidor__dva .btn").first().click();
+            }
+        });
+    };
+};
+
 pageLoad(function(){
     $(document).on('click', '.js_openFormRegistration', openFormRegistration);
+    if ($('.js_regFromLanding').length != 0) {
+        regFromLanding($('.js_regFromLanding'));
+    }
 });
