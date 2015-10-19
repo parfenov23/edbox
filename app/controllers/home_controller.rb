95de9ba6c1aa01c90c0d85de96d2b6daa037422c
maks <prospu@qip.ru>
2015-10-19 18:17:12 +0500

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -166,6 +166,7 @@ class HomeController < ActionController::Base
     begin
       if request.get?
         session[:histories] = [] if !session[:histories].present?
+        session[:histories] = [] if !session[:histories].count > 5
         session[:histories] << request.fullpath if request.fullpath != session[:histories].last
         session[:back_url] = session[:histories].present? ? session[:histories].last(2).first : request.fullpath
       end
