18261a632bef56d23ea725185339c9afd20c6ca6
parfenov23 <parfenov407@gmail.com>
2016-01-11 18:36:07 +0500

    clear тупой метод

--- app/models/incoming_money.rb
+++ app/models/incoming_money.rb
@@ -1,16 +1,4 @@
 class IncomingMoney < ActiveRecord::Base
   serialize :data, ActiveRecord::Coders::Hstore
   belongs_to :user
-
-  def self.create(*args)
-  	binding.pry
-  	hash_params = args.first
-  	im = new
-  	if hash_params.present?
-  		im = new(hash_params)
-  		im.data = hash_params[:data]
-  		im.save
-  	end
-  	im
-  end
 end
