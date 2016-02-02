39a6207a7d1f2e5e989c7622ebbdb5cd7bed5592
maks <prospu@qip.ru>
2016-02-03 00:25:05 +0500

    edit setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'adconsult.online' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "s02.atomsmtp.com",
+    :address              => "smtp-pulse.com",
     :port                 => 2525,
     :domain               => "masshtab.am",
     :user_name            => "prisakar@aim-m.ru",
