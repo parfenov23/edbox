a9c51e5406745ec90cef74984ae438e1f06809e2
maks <prospu@qip.ru>
2016-03-16 17:04:22 +0500

    EK-65 EK-69

--- app/controllers/api/v1/notices_controller.rb
+++ app/controllers/api/v1/notices_controller.rb
@@ -1,5 +1,7 @@
 module Api::V1
   class NoticesController < ::ApplicationController
+    skip_before_action :authorize, only: [:create]
+
     def create
       notice = Notice.new(notice_params)
       notice.save
@@ -8,6 +10,7 @@ module Api::V1
     end
 
     private
+
     def notice_params
       params.require(:notice).permit(:email, :course_id).compact.select { |k, v| v != "" } rescue {}
     end
