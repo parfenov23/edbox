bf052d15ab8872c7989eac0c07d1dbbfdb1939b9
maks <prospu@qip.ru>
2016-07-05 13:01:16 +0500

    fix test free2

--- app/assets/javascripts/core/confirm.js
+++ app/assets/javascripts/core/confirm.js
@@ -50,6 +50,7 @@ var openPopupImgBtn = function () {
     openPopupImg(btn.attr('src'));
 
     var elemPluso = document.querySelectorAll("div.pluso")[0];
+
     elemPluso.pluso.params.url = current_domain() + '/course_cert?id=' + btn.data('id') + '&course_id=' + btn.data('course_id');
     elemPluso.pluso.params.image = current_domain() + btn.attr('src');
     elemPluso.pluso.params.title = "Ура! Я только что получил сертификат ADCONSULT Online!";
