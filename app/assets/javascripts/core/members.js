d26a8df9ec56f62efb30052505d6641b288f8983
maks <prospu@qip.ru>
2016-08-16 03:49:14 +0500

    фикс срипты

--- app/assets/javascripts/core/members.js
+++ app/assets/javascripts/core/members.js
@@ -204,9 +204,10 @@ var installCountResidue = function (n) {
     var count_residue_users = $(".members .count_residue_users");
     if (count_residue_users.length){
         if (n == undefined && n < 0) n = 0;
-        var count = count_residue_users.data('count');
+        var count = count_residue_users.attr('data-count');
         var result_count = count + n;
         count_residue_users.data('count', result_count);
+        count_residue_users.attr('data-count', result_count);
         count_residue_users.text(result_count + " " + declOfNum(result_count, ["приглашение", "приглашения", "приглашений"]));
         return result_count;
     }
