4c40c6fb588149331d7bab94d2214ef10b2c0833
maks <prospu@qip.ru>
2016-10-13 18:38:35 +0500

    fix

--- config/deploy/release.rb
+++ config/deploy/release.rb
@@ -1,6 +1,6 @@
-set :application, 'edbox'
-set :user, 'edbox'
-server '148.251.233.232', user: fetch(:user), roles: %w(web app db)
+set :application, 'adonline'
+set :user, 'adonline'
+server '136.243.79.27', user: fetch(:user), roles: %w(web app db)
 set :rails_env, 'production'
 set :branch, :release
 
