f10b9f966a46eb2f05f6b478aa3a5aa9a159eb79
maks <prospu@qip.ru>
2016-11-13 22:55:26 +0500

    create member project

---
+++ config/deploy/members.rb
@@ -0,0 +1,9 @@
+set :application, 'members_adonline'
+set :user, 'members_adonline'
+server '136.243.79.27', user: fetch(:user), roles: %w(web app db)
+set :rails_env, 'production'
+set :branch, :master
+
+set :deploy_to, "/home/#{ fetch :user }/htdocs"
+set :unicorn_pid, "#{ fetch :deploy_to }/shared/pids/unicorn.pid"
+set :unicorn_config_path, "#{ fetch :deploy_to }/current/config/unicorn/beta.rb"
