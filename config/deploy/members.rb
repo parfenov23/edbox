0f6a36ce56f898615efc6d403206a85af21b9e5d
maks <prospu@qip.ru>
2016-11-14 15:13:16 +0500

    fix deploy

--- config/deploy/members.rb
+++ config/deploy/members.rb
@@ -2,7 +2,7 @@ set :application, 'members_adonline'
 set :user, 'members_adonline'
 server '136.243.79.27', user: fetch(:user), roles: %w(web app db)
 set :rails_env, 'production'
-set :branch, :master
+set :branch, :members
 
 set :deploy_to, "/home/#{ fetch :user }/htdocs"
 set :unicorn_pid, "#{ fetch :deploy_to }/shared/pids/unicorn.pid"
