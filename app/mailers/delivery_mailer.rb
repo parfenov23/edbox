c36351a98b71a23261eaf1f883e76c9e0237b797
maks <prospu@qip.ru>
2017-01-27 00:07:01 +0500

    fix email sender

--- app/mailers/delivery_mailer.rb
+++ app/mailers/delivery_mailer.rb
@@ -1,5 +1,5 @@
 class DeliveryMailer < ActionMailer::Base
-  default :from => "#{$env_mode.name_title} <info@masshtab.am>"
+  default :from => "#{$env_mode.name_title} <support@adconsult.online>"
 
   def send_mail(email, title, description)
     @description = description
