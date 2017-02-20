b20ceff04abc77219683b70c4ac41156ca82d0b7
maks <prospu@qip.ru>
2017-02-20 21:32:52 +0500

    fix bugs

--- config/deploy/release.rb
+++ config/deploy/release.rb
@@ -2,7 +2,7 @@ set :application, 'edbox'
 set :user, 'edbox'
 server '148.251.233.232', user: fetch(:user), roles: %w(web app db)
 set :rails_env, 'production'
-set :branch, :release
+set :branch, :naukacity
 
 set :deploy_to, "/home/#{ fetch :user }/htdocs"
 set :unicorn_pid, "#{ fetch :deploy_to}/shared/pids/unicorn.pid"
