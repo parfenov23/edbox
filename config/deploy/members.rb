31412fa062b5c2cfe43a679f03cc4f596951abda
maks <prospu@qip.ru>
2017-03-07 01:31:27 +0500

    test deploy

--- config/deploy/members.rb
+++ config/deploy/members.rb
@@ -7,3 +7,16 @@ set :branch, :members
 set :deploy_to, "/home/#{ fetch :user }/htdocs"
 set :unicorn_pid, "#{ fetch :deploy_to }/shared/pids/unicorn.pid"
 set :unicorn_config_path, "#{ fetch :deploy_to }/current/config/unicorn/members.rb"
+
+task :copy_latters_shared, roles: :app do
+  run "cp -a #{release_path}/app/views/home_mailers #{shared_path}"
+end
+
+task :shared_copy_latters, roles: :app do
+  run "cp -a #{shared_path} #{release_path}/app/views/home_mailers"
+end
+
+namespace :deploy do
+  after :copy_latters_shared
+  before :shared_cope_latters
+end
