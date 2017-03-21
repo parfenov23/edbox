c882ddbabd3ff33132524e86ee4a4e4a6c9798e8
maks <prospu@qip.ru>
2017-03-21 12:14:20 +0500

    emails view

--- app/mailers/home_mailer.rb
+++ app/mailers/home_mailer.rb
@@ -11,13 +11,19 @@ class HomeMailer < ActionMailer::Base
     @new_password = new_password
     sid = Base64.encode64 "account_id=#{@user.email}"
     @url = "#{$env_mode.current_domain}/sign_up/activate?sid=#{sid}"
-    mail(:to => @user.email, :subject => "Добро пожаловать в #{$env_mode.name_title}!")
+    mail(:to => @user.email, :subject => "Пожалуйста, подтвердите свои данные")
   end
 
   def order_bill(email, params, user)
     @params = params
     @user = user
+    HomeMailer.order_bill_user(user).deliver
     mail(:to => email, :subject => "Заявка на выставление счета")
+  end
+
+  def order_bill_user(user)
+    @user = user
+    mail(:to => user.email, :subject => "Спасибо за заказ")
   end
 
   def email_notifs(email, user)
@@ -27,13 +33,13 @@ class HomeMailer < ActionMailer::Base
 
   def sendRequest(params)
     @params = params
-    mail(:to => 'bazhan@masshtab.am, leads@romanpivovarov.ru, leads@adconsult.club, roman.pivovarov@gmail.com', :subject => "Заявка с Edbox", :reply_to => @params[:email])
+    mail(:to => 'online@naukacity.ru', :subject => "Заявка с Edbox", :reply_to => @params[:email])
   end
 
   def change_password(user, new_password)
     @user = user
     @new_password = new_password
-    mail(:to => @user.email, :subject => "Ваш пароль изменился")
+    mail(:to => @user.email, :subject => "Смена пароля доступа в онлайн-школы Наукасити")
   end
 
   def notice_letter(email, course)
