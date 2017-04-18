d8ad129ea81a700ab6f4c1a30f32a6067b7b086c
maks <prospu@qip.ru>
2017-04-19 00:10:25 +0500

    fix tg

--- app/api_clients/telegram_cli.rb
+++ app/api_clients/telegram_cli.rb
@@ -6,7 +6,9 @@ module ApiClients
     TG_CONFIG = YAML.load_file(File.expand_path("#{Rails.root.to_s}/config/telegram.yml", __FILE__))
 
     def send_message(message)
-      system("#{TG_CONFIG['telegram_path']} -k #{TG_CONFIG['key_path']} -W -e 'msg #{TG_CONFIG['peer']} #{message}'")
+      all_config = "#{TG_CONFIG['telegram_path']} -k #{TG_CONFIG['key_path']} -W -e"
+      peer = "#{TG_CONFIG['peer']}"
+      system('#{all_config} "msg #{peer} \"#{message}\" "')
     end
   end
 end
